041741e0b87c3c50e141cf30f2c889090feb18ad
Rost Alexeev <148265518+R0STUS@users.noreply.github.com>
2024-12-01 14:21:50 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+struct RemindDay {
+    unsigned long time;
+    bool isReminded;
+    std::string name;
+    int typeOfDay;
+};
+
+int main() {
+    std::cout << "Initialising core..." << std::endl;
+}
